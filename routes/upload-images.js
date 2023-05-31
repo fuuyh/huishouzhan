2cb2221f5d15a0d8cfa58290d25a63f91c464af7
fuuyh <1849521093@qq.com>
2023-05-31 10:23:32 +0800

    v2.2

--- routes/upload-images.js
+++ routes/upload-images.js
@@ -4,20 +4,20 @@
 const router = require('express').Router()
 // 引入校验工具
 // 引入 校验规则
-const { orderValidator } = require('../model/orders')
+const { imageValidator } = require('../model/iamges')
 // 引入 validate 
 const validator = require('../middleware/validate')
 // 引入控制器 功能模块
-const orders = require('../controller/orders')
+const image = require('../controller/upload-images')
 // 引入接口鉴权中间件
 const auth = require('../middleware/auth')
 
 
 // 接口设置
 // 添加图片
-router.post('/', [auth, validator(orderValidator)], orders.create)
+router.post('/', [auth, validator(imageValidator)], image.create)
 // 删除单个/批量删除
-router.delete('/:imageId', auth, orders.remove)
+router.delete('/:imageId', auth, image.remove)
 
 // 导出
 module.exports = router
